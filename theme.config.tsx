f15ce5055cb5a905388b9061395709f3e5a56edc
Javian Ng <j4vianz01@gmail.com>
2024-10-25 12:05:03 +0200

    initialise

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>PARAMETRIKS &#123; Documentations &#125;</span>,
+  project: {
+    link: "https://github.com/shuding/nextra-docs-template",
+  },
+  footer: {
+    text: "© 2024 PARAMETRIKS { Documentations }",
+  },
+};
+
+export default config;
