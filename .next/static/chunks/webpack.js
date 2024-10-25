e3c03b627e1819371c9cde9479bed351bd12800d
Javian Ng <j4vianz01@gmail.com>
2024-10-25 15:08:10 +0200

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7aed0e7517870a4"; }
+/******/ 		__webpack_require__.h = function() { return "ae168835a58ca8a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="PARAMETRIKS { Documentations }" />
+      <meta
+        property="og:description"
+        content="Documentations for the PARAMETRIKS"
+      />
+    </>
+  ),
   logo: <span>PARAMETRIKS &#123; Documentations &#125;</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/orgs/gitparametriks/dashboard",
   },
   footer: {
     text: "© 2024 PARAMETRIKS { Documentations }",
